ff9d2213ea06d68ebbe453c1358c0cab048db628
Viviana Montero <viviana.montegue@gmail.com>
2025-01-17 08:26:16 -0600

    actualiazción de libreria activity2 nueva activity2 house se añadieron fragmento 1 y 2

--- app/src/main/java/com/example/miapp/MainActivity2.kt
+++ app/src/main/java/com/example/miapp/MainActivity2.kt
@@ -19,23 +19,6 @@ class MainActivity2 : AppCompatActivity() {
 
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
         }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
-}
+
